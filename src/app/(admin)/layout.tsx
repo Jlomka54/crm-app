844d1c5afd458cb6f31bbb5be9abf38be087ed94
Bohdan Haponiuk <jlomka54@gmail.com>
2025-03-10 08:07:25 +0400

    update layout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -3,21 +3,9 @@ import Sidebar from '@/app/components/sidebar';
 
 export interface LayoutProps {
   children: React.ReactNode;
-  stats: React.ReactNode;
-  sales: React.ReactNode;
-  categories: React.ReactNode;
-  countries: React.ReactNode;
-  promotions: React.ReactNode;
 }
 
-export default function Layout({
-  children,
-  stats,
-  sales,
-  categories,
-  countries,
-  promotions,
-}: LayoutProps) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Sidebar />
